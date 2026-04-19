6f570e3c936637d0b3939a3bcb22c077434483b8
Zephyr Du <zhonghang.or@isrc.iscas.ac.cn>
2026-04-19 21:32:35 +0800

    feature: implment handle import

--- ruyi-handle-rs/src/bin/main.rs
+++ ruyi-handle-rs/src/bin/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+
+use ruyi_handle::input;
+
+fn main() -> Result<(), input::InputError> {
+    let path = Path::new("./package.json");
+    let imported = ruyi_handle::input::from_json_file(path)?;
+
+    println!("Raw: {:?}", imported);
+    println!("Re-jsonify: {}", serde_json::to_string_pretty(&imported)?);
+
+    Ok(())
 }
--- ruyi-handle-rs/src/package.rs
+++ ruyi-handle-rs/src/package.rs
@@ -3,39 +3,43 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize)]
-struct PackageStatic {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PackageStatic {
     name: String,
     version: String,
     release: String,
     license: String,
     url: String,
     vcs: String,
+    #[serde(alias = "buildSystem" /* For KCL */)]
     build_system: String,
-    build_steps: Vec<BuildStep>,
+    #[serde(alias = "build_steps" /* For KCL */)]
+    build_steps: Option<Vec<BuildStep>>,
     sources: HashMap<String, Source>,
     outputs: HashMap<String, Output>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct BuildStep {
     script: String,
     environment: HashMap<String, String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Source {
+    #[serde(alias = "type" /* For KCL */)]
     type_: String,
     url: String,
     checksum: String,
+    #[serde(alias = "checksumType" /* For KCL */)]
     checksum_type: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Output {
     summary: String,
     description: String,
-    requires: String,
-    build_requires: String,
-    files: String,
+    requires: Option<Vec<String>>,
+    build_requires: Option<Vec<String>>,
+    files: Option<Vec<String>>,
 }
