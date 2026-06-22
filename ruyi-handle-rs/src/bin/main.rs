6b30403118d2e7e99b2a82ef92df3f7179f08244
Zephyr Du <zhonghang.or@isrc.iscas.ac.cn>
2026-06-22 15:34:26 +0800

    refactor(handle): rename subcommand build to import

--- ruyi-handle-rs/src/bin/main.rs
+++ ruyi-handle-rs/src/bin/main.rs
@@ -1,8 +1,7 @@
 use std::path::Path;
 
-use askama::Template;
-use clap::{Arg, ArgAction, Command, arg};
-use ruyi_handle::input;
+use clap::{Command, arg};
+use ruyi_handle::{input, package::PackageStatic};
 
 fn main() -> Result<(), input::InputError> {
     let matches = Command::new("ruyi")
@@ -11,38 +10,25 @@ fn main() -> Result<(), input::InputError> {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(
-            Command::new("build")
-                .about("build a file into PackageStatic (JSON)")
+            Command::new("import")
+                .about("Import a file as PackageStatic (JSON), only prints to stdout")
                 .long_about("")
-                .arg(arg!(<FILE>))
-                .arg(
-                    Arg::new("recursive")
-                        .short('r')
-                        .long("recursive")
-                        .help("[TODO]")
-                        .action(ArgAction::SetTrue),
-                )
-                .arg(
-                    Arg::new("debug")
-                        .short('d')
-                        .long("debug")
-                        .help("[TODO] Showing re-exported JSON")
-                        .action(ArgAction::SetTrue),
-                ),
+                .arg(arg!(<FILE>)),
         )
         .subcommand(Command::new("export"))
         .get_matches();
 
     match matches.subcommand() {
-        Some(("build", build_matches)) => {
-            if build_matches.get_flag("recursive") {
-                println!("[Warning] recursive build not implemented.")
-            }
+        Some(("import", import_matches)) => {
+            let path = Path::new(
+                import_matches
+                    .get_one::<String>("FILE")
+                    .expect("This should never panic."),
+            );
+            let ext = path.extension().and_then(|ext| ext.to_str()).unwrap_or("");
 
-            let path = Path::new(build_matches.get_one::<String>("FILE").unwrap());
-            let ext = path.extension().and_then(|e| e.to_str()).unwrap_or("");
-
-            let imported = match ext {
+            // Load the file, according to the file format.
+            let loaded: PackageStatic = match ext {
                 "k" => input::from_kcl_file(path)?,
                 "json" => input::from_json_file(path)?,
                 _ => {
@@ -50,14 +36,7 @@ fn main() -> Result<(), input::InputError> {
                 }
             };
 
-            let rpm_spec = imported.to_rpm_spec_template().unwrap().render().unwrap();
-
-            if build_matches.get_flag("debug") {
-                println!("{}", serde_json::to_string_pretty(&imported)?)
-            };
-            println!("{}", rpm_spec);
-
-            // write("../temp/graphviz.spec", rpm_spec).unwrap();
+            println!("{}", serde_json::to_string_pretty(&loaded)?)
         }
         _ => unreachable!(),
     }
